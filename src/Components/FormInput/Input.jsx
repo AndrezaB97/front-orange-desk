093ca1ef3332f45a04c289c40855c15e49ffe6ef
Henrique Rinaldi <henriquergr@hotmail.com>
2021-09-07 16:48:42 -0300

    refactor: adjust style

--- src/Components/FormInput/Input.jsx
+++ src/Components/FormInput/Input.jsx
@@ -2,21 +2,29 @@ import React, { useState } from 'react';
 import { IoMdEye, IoMdEyeOff } from 'react-icons/io'
 
 import '../../style/global-style.css';
-import './Input.css';
+// import './Input.css';
 
 
-const Input = ({ type, placeholder, name }) => {
+const Input = ({ type, placeholder, name, className }) => {
 
     const [toggle, setToggle] = useState(false)
 
     return (
-        <div className='input-container'>
-            <input type={!toggle ? type : 'text'} placeholder={placeholder} {...name} />
+        <div className='input-group border rounded mt-2'>
+            <input type={!toggle ? type : 'text'} className={className} placeholder={placeholder} {...name} />
             {type === 'password' && (
                 toggle ?
-                    <IoMdEye size='2.5rem' color='#000' opacity='60%' cursor='pointer' onClick={() => setToggle(false)} />
+                    (
+                        <span className='input-group-text bg-transparent border-0'>
+                            <IoMdEye size='1.5rem' color='#000' opacity='60%' cursor='pointer' onClick={() => setToggle(false)} />
+                        </span>
+                    )
                     :
-                    <IoMdEyeOff size='2.5rem' color='#000' opacity='60%' cursor='pointer' onClick={() => setToggle(true)} />
+                    (
+                        <span className='input-group-text bg-transparent border-0'>
+                            <IoMdEyeOff size='1.5rem' color='#000' opacity='60%' cursor='pointer' onClick={() => setToggle(true)} />
+                        </span>
+                    )
                 )
             }
         </div>
