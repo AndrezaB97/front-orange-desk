456bde8a354fa360abf7f88cbb7970067bf4c78d
Henrique Rinaldi <henriquergr@hotmail.com>
2021-09-08 18:10:12 -0300

    refactor: responsive adjustments

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -15,6 +15,7 @@ import Logo from '../../Components/Logo/Logo'
 // Importing style-sheets:
 import '../../style/global-style.css';
 import './Register.css'
+import ConsultorHeader from '../../Components/ConsultorHeader/ConsultorHeader';
 
 
 function Register() {
@@ -41,17 +42,14 @@ function Register() {
   return (
     <React.Fragment>
 
-      <div className='container vh-100 d-flex flex-column'>
-        <div className="container mt-3">
-          <div className="row justify-content-between">
-            <div className="col-1">
-              <Link to='/'> <Logo /> </Link>
-            </div>
-            <div className="col-1 h-auto bg-primary"></div>
-          </div>
-        </div>
-        <div className="container">
-          <div className="row">
+      <div className="container-fluid d-flex flex-column align-items-center vh-100">
+        <ConsultorHeader currentStep={'adm'} />
+        {/* <div className="row align-self-start">
+        </div> */}
+
+
+        <div className="container d-flex align-items-center vh-100">
+          <div className="row flex-grow-1">
             <div className="col-md-6 col-12">
               <h3>Estamos quase lá!</h3>
               <p>Só precisamos de mais algumas informações.</p>
@@ -63,13 +61,12 @@ function Register() {
                 <Input type={'password'} className={'form-control border-0 shadow-none'} placeholder={'Senha'} name={register('password')} />
                 <Input type={'password'} className={'form-control border-0 shadow-none'} placeholder={'Confirme a senha'} name={register('confirmPassword')} />
               </form>
-              <button id='btnRegister' className='btn w-100 h-auto mt-3 d-flex flex-column justify-content-end' onClick={save}>Cadastrar e continuar</button>
-              <ToastContainer />
+              <button id='btnRegister' className='btn w-100 h-auto mt-3 align-self-end' onClick={save}>Cadastrar e continuar</button>
             </div>
           </div>
         </div>
       </div>
-
+      
     </React.Fragment>
   )
 }
