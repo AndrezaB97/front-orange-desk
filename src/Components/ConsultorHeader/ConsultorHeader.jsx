a48a07e6d11bdc6873bc687ebb844600f9c58627
Henrique Rinaldi <henriquergr@hotmail.com>
2021-09-10 00:35:59 -0300

    refactor: changed svg icon to a react-icon

--- src/Components/ConsultorHeader/ConsultorHeader.jsx
+++ src/Components/ConsultorHeader/ConsultorHeader.jsx
@@ -2,18 +2,20 @@ import React from 'react';
 import { useHistory } from 'react-router-dom';
 
 import StepBar from '../StepBar/StepBar';
-import BackButton from '../.././Assets/arrow-left.svg';
+import { FiArrowLeftCircle } from 'react-icons/fi'
 
 const ConsultorHeader = ({ currentStep }) => {
 
     const history = useHistory();
 
-    return ( 
+    return (
 
         <>
             <header className='row container-fluid d-flex flex-column justify-content-between mt-2'>
                 <nav className='row d-flex justify-content-between mb-5'>
-                    <button className='col-3 p2 bg-white' onClick={() => history.push('/')}> <img className='img-fluid' src={ BackButton } alt='Botão para retornar à página anterior.'/> </button>
+                    <button className='col-3 p2 bg-white'>
+                        <FiArrowLeftCircle size='24' color='#6A6A6A' onClick={() => history.push('/')}/>
+                    </button>
                     <div className='col-1 p-2'>Logo</div>
                 </nav>
 
