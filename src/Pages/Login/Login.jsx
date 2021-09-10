386ab90d7b98f80cd30d6ae454cdecee433d8829
Henrique Rinaldi <henriquergr@hotmail.com>
2021-09-09 22:27:26 -0300

    feat: implemented ux-design to Login page

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,21 +1,24 @@
 // Importing modules:
-import React, { useEffect, useState, setUser } from 'react'
+import React from 'react'
 import { ToastContainer, toast } from 'react-toastify'
 import { useForm } from 'react-hook-form'
-import { Link } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
 
 // Importing services:
 import api from '../../services/api'
 
 // Import components:
 import Input from '../../Components/FormInput/Input'
+import { FiArrowLeftCircle } from 'react-icons/fi'
 
 // Importing style-sheets:
 import '../../style/global-style.css';
-import ConsultorHeader from '../../Components/ConsultorHeader/ConsultorHeader';
+import './Login.css'
 
 const Login = () => {
     const { handleSubmit, register, formState: { errors } } = useForm()
+
+    const history = useHistory()
 
     const onSubmit = async data => {
         try {
@@ -34,21 +37,28 @@ const Login = () => {
     return (
         <React.Fragment>
 
-            <div className="container-fluid d-flex flex-column align-items-center vh-100">
-                <form className="row w-100 h-100 my-3" onSubmit={handleSubmit(onSubmit)}>
-                    <div className="col-md-6 col-12 bg-primary">
-                        <h5>Faça seu login!</h5>
+            <main className="container-fluid d-flex flex-column align-items-center vh-100">
+                <header className='container d-flex justify-content-between mt-3'>
+                    <button className='bg-white'>
+                        <FiArrowLeftCircle size='24' onClick={() => history.push('/')}/>
+                    </button>
+                    <div>Logo</div>
+                </header>
+                <form className="row w-auto h-100 align-content-md-center my-3 mx-1" onSubmit={handleSubmit(onSubmit)}>
+                    <div className="col-12">
+                        <h5>Faça seu login.</h5>
                         <p>Só precisamos de mais algumas informações.</p>
-                        <Input type={'email'} className={'form-control border-0 shadow-none'} placeholder={'Email *'} name={register('email', { required: true })} />
+                        <Input type={'email'} className={'form-control border-0 shadow-none'} placeholder={'Email'} name={register('email', { required: true })} />
                         {errors.email && errors.email.type === "required" && <span className="text-danger">Campo obrigatório</span>}
-                        <Input type={'password'} className={'form-control border-0 shadow-none'} placeholder={'Senha *'} name={register('password', {required: true})} />
+                        <Input type={'password'} className={'form-control border-0 shadow-none'} placeholder={'Senha'} name={register('password', { required: true })} />
                         {errors.password && errors.password.type === "required" && <span className="text-danger">Campo obrigatório</span>}
                     </div>
-                    <div className="col-md-6 col-12 bg-secondary align-self-end text-center">
-                        <button className='w-100'>Entrar</button>
+                    <div className="col-12 align-self-end text-center mt-md-3">
+                        <button id='btnLogin' className='form-control w-100 rounded border-0'>Entrar</button>
+                        <ToastContainer/>
                     </div>
                 </form>
-            </div>
+            </main>
 
         </React.Fragment>
     )
