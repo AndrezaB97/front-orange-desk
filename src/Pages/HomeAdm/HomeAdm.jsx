94e3e12825e3d04db9439a02ff99a3572a13cca3
João Victor P. Rodrigues <jvpr123@outlook.com>
2021-09-10 16:30:47 -0300

    Responsibility adjusted on pages Home and Consultor.

--- src/Pages/HomeAdm/HomeAdm.jsx
+++ src/Pages/HomeAdm/HomeAdm.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import Img from './../../Assets/home-page.svg';
+import Logo from './../../Assets/Logo-FCamara.png';
 
 const HomeAdm = () => {
   return(
@@ -9,20 +10,22 @@ const HomeAdm = () => {
     <React.Fragment>
       <div className="container-lg container-md d-flex flex-column justify-content-start align-items-center bg-white container vw-100 vh-100">
 
-          <header className=' col-lg-10 container-fluid d-flex justify-content-between align-items-center mt-2 mb-4 mt-lg-4'>
-            <div className='col-3 p-2'>Logo</div>
-            <Link to='#' className='text-orange fw-bold col-2'>Login</Link>
+          <header className='col-lg-10 col-11 container-fluid d-flex justify-content-between align-items-center mt-3 mb-4 mt-lg-4'>
+            <Link className='col-6 col-md-2' to='/'>
+              <img className='img-fluid p-2' src={ Logo } alt='Logotipo FCamara'/>
+            </Link>
+            <Link to='#' className='text-orange link-hover fw-bold col-1'>Login</Link>
           </header>
 
-          <main className= 'container-fluid row v-100 d-flex justify-content-start flex-column align-items-center flex-lg-row'> 
-            <img className="img-fluid w-md-25 col-lg-6" src={Img} alt="Pessoas sentadas"/>
+          <main className= 'container-fluid row w-100 d-flex justify-content-start flex-column align-items-center flex-lg-row'> 
+            <img className="img-fluid col-10 col-lg-6" src={Img} alt="Duas mulheres sentadas no escritório conversando e tomando café."/>
 
-            <div className="d-flex flex-column col-md-6 col-lg-5 col-12">
+            <div className="d-flex flex-column col-md-9 col-lg-5 col-12 mt-2">
               <h5 className='mb-1'>Bateu saudade dos colegas?</h5>
-              <h5>Precisa fazer uma reunião importante?</h5>
-              <p className='mt-4'>Seja qual for o motivo, aqui você agenda o dia que precisa ir ao escritório e garante um dia de trabalho seguro e agradável</p>
+              <h5 className=''>Precisa fazer uma reunião importante?</h5>
+              <p className='mt-3'>Seja qual for o motivo, aqui você agenda o dia que precisa ir ao escritório e garante um dia de trabalho seguro e agradável.</p>
 
-              <Link className="btn-orange mt-4 w-100 " to='/consultor'>
+              <Link className="btn-orange mt-4 w-100 col-12" to='/consultor'>
                 Quero reservar
               </Link>
             </div>
