3d7a513fdd654fa5e127c724d283a195c8fe1101
Henrique Rinaldi <henriquergr@hotmail.com>
2021-09-09 22:20:37 -0300

    refactor: changed <a> tag to Link component

--- src/Pages/HomeAdm/HomeAdm.jsx
+++ src/Pages/HomeAdm/HomeAdm.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import Img from './../../Assets/home-adm.png';
+import { Link } from 'react-router-dom';
 
 
 const HomeAdm = () => {
-  return(
+  return (
     <React.Fragment>
       <div className="container">
         <div className="row vh-100 d-flex align-items-center">
 
           <div className="d-flex justify-content-end">
-            <a href="/adm/criar" className="login-text p-3">Minha Empresa</a>
-            <a href="#" className="login-text p-3">Login</a>
+            <Link to="/adm/criar" className="login-text p-3">Minha Empresa</Link>
+            <Link to='/login' className='login-text p-3' >Login</Link>
           </div>
 
 
@@ -20,7 +21,7 @@ const HomeAdm = () => {
 
           <div className="col-md-6 col-12">
             <h5>Bateu saudade dos colegas? Precisa fazer uma reunião importante?</h5>
-            
+
             <p>asdasakjsfjaksdlfa Seja qual for o motivo, aqui você agenda o dia que precisa ir ao escritório e garante um dia de trabalho seguro e agradável.</p>
 
             <button className="btn-orange mt-3 w-100">Quero reservar</button>
