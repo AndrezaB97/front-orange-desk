fcd157ddafa3a793027b96a6c75a73dc6d784318
Henrique Rinaldi <henriquergr@hotmail.com>
2021-09-12 19:02:28 -0300

    feat: finished mobile structure, design and validation

--- src/Pages/SelectDesk/SelectDesk.jsx
+++ src/Pages/SelectDesk/SelectDesk.jsx
@@ -1,25 +1,37 @@
 // Importing modules:
-import React from 'react'
-import { Link, useHistory } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
 
 // Import components:
-import { FiArrowLeftCircle } from 'react-icons/fi'
 import ConsultorHeader from '../.././Components/ConsultorHeader/ConsultorHeader'
 import DesksLayoutImg from './../../Assets/desks-layout.svg'
-import Input from './../../Components/FormInput/Input'
 
 // Importing style-sheets:
 
 const SelectDesk = () => {
 
     const inputStyle = {
-        backgroundColor: '#F4F4F4',
-        borderColor: '#F2F2F2'
+        backgroundColor: '#F4F4F4'
     }
 
     const accordionHeader = {
         color: '#404099'
     }
+
+    const [value, setValue] = useState('')
+    const HandleChange = (e) => setValue(e.target.value)
+
+    useEffect(() => {
+        const btnSelectDesk = document.getElementById('btnSelectDesk')
+        if ((value !== '' && value !== undefined) && value.length <= 2 && (Number(value) >= 1 && Number(value) <= 40)) {
+            btnSelectDesk.removeAttribute('disabled')
+            btnSelectDesk.setAttribute('style', 'background-color: #FE4400; color: white')
+        }
+        else {
+            btnSelectDesk.setAttribute('disabled', '')
+            btnSelectDesk.setAttribute('style', 'background-color: #EAEAEA; color: #6A6A6A')
+        }
+    }, [HandleChange])
 
     return (
         <React.Fragment>
@@ -27,34 +39,38 @@ const SelectDesk = () => {
             <div className="container-fluid">
                 <ConsultorHeader currentStep={'desk'} />
 
-                <div className="container mt-3">
-                    <h5>Escolha sua estação de trabalho.</h5>
+                <div className="container my-3 text-center">
+                    <p>Escolha sua estação de trabalho.</p>
                     <img className='img-fluid' src={DesksLayoutImg} alt="Imagem com layout das mesas" />
                     <form className='mt-3'>
-                        <input
-                            type='number'
-                            name=''
-                            id=''
+                        <input onChange={HandleChange}
+                            type='text'
                             style={inputStyle}
-                            className='form-control border-0 shadow-none'
-                            placeholder='Digite o nº da mesa'
-                            min='1'
-                            max='40'
+                            className=' text-center form-control border-0 shadow-none'
+                            placeholder='Digite o nº da mesa (01 a 40)'
+                            maxLength={2}
                             required />
+                            {(value !=='' && Number(value) < 1 || Number(value) > 40) && <span className='text-danger'>Valor deve estar entre 1 e 40</span>}
 
-                    </form>
-                    <div class="accordion accordion-flush mt-3" id="accordionFlush">
-                        <div class="accordion-item text-center">
-                            <p class="accordion-header" id="flush-showDesks">
-                                <button style={accordionHeader} class="collapsed bg-white" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
-                                    Mostrar mesas disponíveis
-                                </button>
-                            </p>
-                            <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-showDesks" data-bs-parent="#accordionFlush">
-                                <div class="accordion-body">01 | 02 | 03</div>
+                        <div class="accordion accordion-flush mt-3 mb-5" id="accordionFlush">
+                            <div class="accordion-item text-center">
+                                <p class="accordion-header" id="flush-showDesks">
+                                    <button style={accordionHeader} class="collapsed bg-white shadow-none" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                                        Mostrar mesas disponíveis
+                                    </button>
+                                </p>
+                                <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-showDesks" data-bs-parent="#accordionFlush">
+                                    <div class="accordion-body">01 | 02 | 03</div>
+                                </div>
                             </div>
                         </div>
-                    </div>
+
+                        <Link to='/' className='text-decoration-none'>
+                            <button id='btnSelectDesk' className='form-control border-0 shadow-none' disabled >
+                                Escolher estação de trabalho
+                            </button>
+                        </Link>
+                    </form>
                 </div>
 
             </div>
