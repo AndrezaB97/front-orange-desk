ce8903e3148ea5459e827a30bbadac699750cde4
Henrique Rinaldi <henriquergr@hotmail.com>
2021-09-12 00:36:08 -0300

    feat: initiated SelectDesk page

---
+++ src/Pages/SelectDesk/SelectDesk.jsx
@@ -0,0 +1,18 @@
+// Importing modules:
+import React from 'react'
+import { Link, useHistory } from 'react-router-dom'
+
+// Import components:
+import { FiArrowLeftCircle } from 'react-icons/fi'
+
+// Importing style-sheets:
+
+const SelectDesk = () => {
+    return (
+        <React.Fragment>
+
+        </React.Fragment>
+    )
+}
+
+export default SelectDesk;
