d87420e9b869a5a1b117d7edf5346460bc4f8dd0
Henrique Rinaldi <henriquergr@hotmail.com>
2021-09-14 00:09:39 -0300

    refactor: implemented checkbox input properly

--- src/Pages/Review/Review.jsx
+++ src/Pages/Review/Review.jsx
@@ -7,21 +7,22 @@ import ConsultorHeader from '../../Components/ConsultorHeader/ConsultorHeader';
 const Review = () => {
 
     document.title = 'REVIEW | Orange Desk';
-    
-    const { handleSubmit, register, formState: { errors } } = useForm();
 
-    return ( 
+    const { handleSubmit, register, formState: { errors } } = useForm();
+    const onSubmit = data => console.log(data);
+
+    return (
 
         <React.Fragment>
             <div className="container-lg container-md d-flex flex-column justify-content-start bg-white container vw-100 vh-100">
 
-                <ConsultorHeader currentStep={ 'review' } />
+                <ConsultorHeader currentStep={'review'} />
 
-                <main clasName= 'container-lg row w-100 d-flex justify-content-start flex-column align-items-center flex-lg-row'>
-                    
+                <main clasName='container-lg row w-100 d-flex justify-content-start flex-column align-items-center flex-lg-row'>
+
                     <div className='col-lg-6 col-md-10 ms-auto me-auto'>
                         <p className='text-black mt-4 mt-lg-5 text-center'>Confira as informações</p>
-                
+
                         <div className='mt-4 ms-4'>
                             <h4 className='text-black fw__medium'>Jéssica da Silva</h4>
                             <p className='text-grey fw__light'>jessicasilva@fcamara.com.br</p>
@@ -42,25 +43,31 @@ const Review = () => {
                                 <p className='text-grey fw__light'>unidade</p>
                                 <p className='text-black'>Sede - São Paulo</p>
                                 <p className='text-black fs-12 fw__light'>
-                                    Rua Bela Cintra, 986 - 2° andar<br/>
+                                    Rua Bela Cintra, 986 - 2° andar<br />
                                     Consolação, São Paulo - SP
                                 </p>
                             </div>
 
                             <div>
-                                <form className='mt-5' onSubmit={ handleSubmit() }>
-                                    <div className='d-flex'>
-                                        <input type={'checkbox'}
-                                                value={ true } 
-                                                className={'form-control me-4 fw__light checkbox-size shadow-none'} 
-                                                name={register('checkbox', { required: true })} />
-                                        <label className='text-grey fs-10 fw__light d-flex justify-space-between'>
-                                            Eu me comprometo a respeitar as regras de distanciamento e os demais protocolos com o objetivo de combater a disseminação do coronavírus.
-                                            {errors.password && errors.password.type === "required" && <span className="text-danger">Campo obrigatório</span>}
-                                        </label>
-                                    
-                                    </div>            
-                        
+                                <form className='mt-5' onSubmit={handleSubmit(onSubmit)}>
+                                    <div className='container p-0'>
+                                        <div className="row align-items-center">
+                                            <div className="col-2">
+                                                <input type='checkbox'
+                                                    id='btnCheck'
+                                                    className={'fw__light checkbox-size shadow-none'} 
+                                                    {...register('teste', { required: true })} />
+                                            </div>
+                                            <div className="col-10">
+                                                <label for='' className='text-grey fs-10 fw__light'>
+                                                    Eu me comprometo a respeitar as regras de distanciamento e os demais protocolos com o objetivo de combater a disseminação do coronavírus.
+                                                </label>
+                                            </div>
+                                        </div>
+
+                                    </div>
+                                    {errors.teste && errors.teste.type === "required" && <span className="text-danger">Campo obrigatório</span>}
+
                                     <button id='btnRegister' className='btn-orange w-100 mt-3 mb-2'>Confirmar agendamento</button>
                                 </form>
                             </div>
@@ -72,7 +79,7 @@ const Review = () => {
             </div>
         </React.Fragment>
 
-     );
+    );
 }
- 
+
 export default Review;
