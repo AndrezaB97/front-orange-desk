ba44baeb434ff701bb7501a3f65ea12e926a0044
AndrezaB97 <andreza.adsb@gmail.com>
2021-09-16 23:02:17 -0300

    fix build

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-import App from './App';
+import App from './App'
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle';
-import './style/index.css';
-import './style/font-pack/Metropolis-font-face-kit.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle'
+import './style/index.css'
+import './style/font-pack/Metropolis-font-face-kit.css'
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
